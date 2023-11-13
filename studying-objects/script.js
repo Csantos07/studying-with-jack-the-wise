2ad09d159730a6f2b12fef7432c008089d888b06
Carlo Santos <rcarlosantos89@gmail.com>
2023-11-12 19:06:35 -0600

    creating a person object

--- studying-objects/script.js
+++ studying-objects/script.js
@@ -1,2 +1,15 @@
 // Creating an object
-console.log('It\'s working!');
+
+let person = {
+  name: "John",
+  age: 31,
+  favColor: "green",
+  height: 183,
+  languages: ["C++", "Python", "JavaScript"],
+  sayLanguagesLearned: function () {
+    console.log(`Hello, I am learning: ${this.languages}"!`);
+  }
+};
+
+
+console.log(person.sayLanguagesLearned());
